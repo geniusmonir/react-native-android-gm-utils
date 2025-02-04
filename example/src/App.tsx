10ebdb37eb4ce0322b75a9d9180fa50692990f08
Md. Moniruzzaman <monir.jbt@gmail.com>
2025-02-04 13:14:15 +0600

    fix: fixes lint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,12 @@
 import {
-  multiply,
   getNotificationPermissionStatus,
   requestNotificationPermission,
-  addNumbers,
 } from 'react-native-android-gm-utils';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { useState, useEffect } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { useEffect } from 'react';
 
 export default function App() {
-  const [result, setResult] = useState<number | undefined>();
-  const [resultA, setResultA] = useState<number | undefined>();
-
   useEffect(() => {
-    multiply(3, 7).then(setResult);
-
-    addNumbers(3, 7).then(setResultA);
-
     const getStatus = async () => {
       const status = await getNotificationPermissionStatus();
       console.log('The status is: ', status);
@@ -25,9 +16,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <Text>Result A: {resultA}</Text>
-
       <Button
         title="Request Permission"
         onPress={async () => {
