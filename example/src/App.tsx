0d5b894a81c49d96432c78e8d6df895d6a4e2017
Md. Moniruzzaman <monir.jbt@gmail.com>
2025-08-26 03:36:35 +0600

    fix: example app fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,10 @@
 import {
-  getNotificationPermissionStatus,
   isBatteryOptimizationEnabled,
   requestDisableBatteryOptimization,
 } from 'react-native-android-gm-utils';
 import { View, StyleSheet, Button } from 'react-native';
-import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    const getStatus = async () => {
-      const status = await getNotificationPermissionStatus();
-      console.log('The status is: ', status);
-    };
-    getStatus();
-  }, []);
-
   return (
     <View style={styles.container}>
       <Button
